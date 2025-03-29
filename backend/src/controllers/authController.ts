801ce502ef7f304f27655f0176510e8d6a96182f
uday-bhatiya <udaybhatiya31@gmail.com>
2025-03-29 13:14:18 +0530

    add user register endpoint

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import prisma from "../config/prisma";
+
+const register = async (req: Request, res: Response): Promise<void> => {
+    try {
+        // get data
+        const { email, password } = req.body;
+
+        //check fields
+        if (!email || !password) {
+            res.status(400).json({
+                success: false,
+                message: "Email and password are required"
+            });
+            return;
+        }
+
+        //check email, password
+        if (typeof email !== "string" || !email.includes("@")) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid email format"
+            });
+            return;
+        }
+
+        if (typeof password !== "string" || password.length < 8) {
+            res.status(400).json({
+                success: false,
+                message: "Password must be atleast 8 characters long"
+            });
+            return;
+        }
+
+        //check user
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+        if (existingUser) {
+            res.status(400).json({
+                success: false,
+                message: "User already exists"
+            });
+            return;
+        }
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword
+            }
+        });
+
+        // console.log(newUser);
+
+        //generate token
+        const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET!, { expiresIn: "24h" });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            token
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+}
+
+export { register }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import dotenv from "dotenv";
+import cors from "cors";
+import authRouter from "./routes/authRoutes";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
 app.use(express());
+app.use(express.json())
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server is running!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Express + TypeScript Server is running!");
+// });
+
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
